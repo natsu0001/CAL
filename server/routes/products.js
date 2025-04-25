e771289ddd9762737d422d4322383eaebdd2d4b7
natsu0001 <talukdarabhishek687@gmail.com>
2025-04-25 09:50:06 +0530

    servTst

---
+++ server/routes/products.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Product from '../models/Product.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+router.post('/', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.status(201).json(newProduct);
+});
+
+
+
+export default router;
---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import productRoutes from './routes/products.js';
+
+dotenv.config(); 
+const app = express(); 
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/products', productRoutes); 
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((err) => console.error(err));
