d2e9806b71e89dfa854b7ee5fab4e4ab3bea33c8
natsu0001 <talukdarabhishek687@gmail.com>
2025-05-06 09:39:44 +0530

    GoogLGaded

--- animelist-client/src/firebase/firebase.js
+++ animelist-client/src/firebase/firebase.js
@@ -2,10 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyApforGDq9sZ71VtRzbOvgbAC0Jep-Hs1U",
+  authDomain: "anilist-3b5c3.firebaseapp.com",
+  projectId: "anilist-3b5c3",
+  storageBucket: "anilist-3b5c3.firebasestorage.app",
+  messagingSenderId: "635439066916",
+  appId: "1:635439066916:web:ee085c6f96cba849104db1",
+  measurementId: "G-1BYGYW35TS"
 };
 
 const app = initializeApp(firebaseConfig);
