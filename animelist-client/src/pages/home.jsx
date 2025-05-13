814f78c6b8d8df4dc716fb0938d4efbff8444396
natsu0001 <talukdarabhishek687@gmail.com>
2025-05-13 12:55:53 +0530

    ..

--- animelist-client/src/pages/home.jsx
+++ animelist-client/src/pages/home.jsx
@@ -11,25 +11,25 @@ import UpcomingAnimeCarousel from "../components/UpcomingAnimeCarousel";
 const Home = () => {
   return (
         <div style={{ backgroundColor: '#110F1F' }} className="min-h-screen text-white">
-      {/* Constrain and center the whole content */}
+      
 <div className="max-w-screen-3xl mx-auto px-4">
               
-        {/* Trending Season Carousel */}
+        
         <div className="overflow-x-auto mt-0">
           <TrendingSeasonCarousel />
         </div>
 
-        {/* Trending Anime Carousel */}
+       
         <div className="mt-8">
           <TrendingAnimeCarousel />
         </div>
 
-        {/* Upcoming Anime Carousel */}
+        
         <div className="mt-8">
           <UpcomingAnimeCarousel />
         </div>
 
-        {/* News Section */}
+       
         <div className="mt-8">
           <AnimeNewsSection />
         </div>
